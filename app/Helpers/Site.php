2895212eeaca6a55c58b344eb049b47f4e35e6b1
FahathAMM <fahathammex90@gmail.com>
2025-05-17 20:23:24 +0400

    Update Site.php

--- app/Helpers/Site.php
+++ app/Helpers/Site.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Models\Category\Category;
+use App\Models\Product\Product;
 
 if (!function_exists('getCategories')) {
     function getCategories()
     {
 
         return Category::with('subcategories')->get();
+    }
+}
+
+if (!function_exists('getProductsForCardModel')) {
+    function getProductsForCardModel()
+    {
+        return Product::get();
     }
 }
