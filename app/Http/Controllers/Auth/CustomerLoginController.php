7d48f92d8b7c07ce4ff4326ae5e6ff18e5732a6e
FahathAMM <fahathammex90@gmail.com>
2025-05-17 20:23:34 +0400

    Update CustomerLoginController.php

--- app/Http/Controllers/Auth/CustomerLoginController.php
+++ app/Http/Controllers/Auth/CustomerLoginController.php
@@ -8,12 +8,18 @@ use App\Models\Customer\Customer;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\StoreRequest;
 
 class CustomerLoginController extends Controller
 {
     public function showLoginForm()
     {
-        return view('auth.customer-login');
+        return view('site.auth.customer-login');
+    }
+
+    public function showRegisterForm()
+    {
+        return view('site.auth.customer-register');
     }
 
     public function login(Request $request)
@@ -27,11 +33,33 @@ class CustomerLoginController extends Controller
 
         if ($customer && Hash::check($request->password, $customer->password)) {
             Auth::guard('customer')->login($customer);
-            return redirect()->intended('/customer/dashboard');
+            return redirect()->intended('/');
         }
 
         return back()->withErrors(['mobile' => 'Invalid mobile number or password']);
     }
+
+    public function register(StoreRequest $request)
+    {
+        try {
+            $created  = Customer::create([
+                'name' => $request->name,
+                'mobile' => $request->mobile,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            if ($created) {
+                logActivity('Customer Create', "Customer ID " . $created->id, 'Create');
+                // Auth::guard('customer')->login($created);
+                return  $this->response('Customer created successfully', ['data' => $created], true);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+            return  $this->response($th->getMessage(), null, false);
+        }
+    }
+
 
     public function logout()
     {
