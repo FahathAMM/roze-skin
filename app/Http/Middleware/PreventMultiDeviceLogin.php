aed2c235c4ef0b388c542fb73a194f223d8a0b9f
fahathAMM <fahathammex90@gmail.com>
2025-07-03 22:17:49 +0400

    Update PreventMultiDeviceLogin.php

--- app/Http/Middleware/PreventMultiDeviceLogin.php
+++ app/Http/Middleware/PreventMultiDeviceLogin.php
@@ -12,14 +12,17 @@ class PreventMultiDeviceLogin
 {
     public function handle(Request $request, Closure $next)
     {
-        $user = Auth::user();
-        if (($user && $user->session_id && $user->session_id !== session()->getId() || $user->session_id == null)) {
-            Auth::logout();
-            return redirect('/login')->withErrors([
-                'username' => 'You have been logged out because your account was accessed from another device.',
-            ]);
+        if ($isEnableMultiAuth = false) {
+            $user = Auth::user();
+            if (($user && $user->session_id && $user->session_id !== session()->getId() || $user->session_id == null)) {
+                Auth::logout();
+                return redirect('/login')->withErrors([
+                    'username' => 'You have been logged out because your account was accessed from another device.',
+                ]);
+            }
+            return $next($request);
+        } else {
+            return $next($request);
         }
-
-        return $next($request);
     }
 }
