2ccb93fef66e4641f3a2bd21f8ff494454376fc3
fahathAMM <fahathammex90@gmail.com>
2025-07-03 22:19:09 +0400

    Update MenusTableSeeder.php

--- database/seeders/MenusTableSeeder.php
+++ database/seeders/MenusTableSeeder.php
@@ -5,15 +5,10 @@ namespace Database\Seeders;
 use App\Models\Menu\MenuDetail;
 use App\Models\Menu\MenuHeader;
 use Illuminate\Database\Seeder;
-use App\Models\Menu\MenuSubDetail;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class MenusTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         DB::table('menu_headers')->truncate();
@@ -23,199 +18,212 @@ class MenusTableSeeder extends Seeder
         $currentDateTime = now();
         $prefix = 'admin';
 
-        $menuHeaderDashboard = MenuHeader::create([
-            'name1' => 'Dashboards',
-            'name2' => 'Dashboards',
-            'is_active' => 1,
-            'icon' => 'ri-dashboard-2-line',
-            'menu_slug' => null,
-            'menu_code' => 1,
-            'menu' => null,
-            'created_at' => $currentDateTime,
-            'updated_at' => $currentDateTime,
-        ]);
-
-        $menuHeaderOrganization = MenuHeader::create([
-            'name1' => 'Organization',
-            'name2' => 'Organization',
-            'is_active' => 1,
-            'icon' => 'ri-dashboard-2-line',
-            'menu_slug' => null,
-            'menu_code' => 100,
-            'menu' => null,
-            'created_at' => $currentDateTime,
-            'updated_at' => $currentDateTime,
-        ]);
-
-        $menuHeaderAdministration = MenuHeader::create([
-            'name1' => 'Administration',
-            'name2' => 'Administration',
-            'is_active' => 1,
-            'icon' => 'ri-admin-line',
-            'menu_slug' => null,
-            'menu_code' => 300,
-            'menu' => json_encode(['item1' => 'value1', 'item2' => 'value2']),
-            'created_at' => $currentDateTime,
-            'updated_at' => $currentDateTime,
-        ]);
-
-        $menuDetails = [
+        $menuItems = [
             [
-                'menu_header_id' => $menuHeaderOrganization->id,
-                'name1' => 'Category',
-                'name2' => 'Category',
-                'sequence' => '10',
-                'menu_slug' => '',
-                'page_url' => "$prefix/category",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-download-cloud-2-fill',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderOrganization->id,
-                'name1' => 'Product',
-                'name2' => 'Product',
-                'sequence' => '20',
-                'menu_slug' => '',
-                'page_url' => "$prefix/products",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-arrow-go-back-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderOrganization->id,
-                'name1' => 'Solution',
-                'name2' => 'Solution',
-                'sequence' => '1',
-                'menu_slug' => '',
-                'page_url' => "$prefix/solution",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-shopping-bag-3-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
+                'header' => [
+                    'name1' => 'Dashboards',
+                    'name2' => 'Dashboards',
+                    'is_active' => 1,
+                    'icon' => 'ri-dashboard-2-line',
+                    'menu_slug' => null,
+                    'menu_code' => 1,
+                    'menu' => null,
+                    'created_at' => $currentDateTime,
+                    'updated_at' => $currentDateTime,
+                ],
+                'children' => []
             ],
 
+            // ['title' => 'Products', 'route' => 'admin.products.index'],
+            // ['title' => 'Add New Product', 'route' => 'admin.products.create'],
+            // ['title' => 'Categories', 'route' => 'admin.categories.index'],
+            // ['title' => 'Brands', 'route' => 'admin.brands.index'],
+            // ['title' => 'Attributes', 'route' => 'admin.attributes.index'],
+            // ['title' => 'Tags', 'route' => 'admin.tags.index'],
+            // ['title' => 'Inventory', 'route' => 'admin.inventory.index'],
+
             [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Users',
-                'name2' => 'Users',
-                'sequence' => '1',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/user",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-user-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
+                'header' => [
+                    'name1' => 'Product-Management',
+                    'name2' => 'Product-Management',
+                    'is_active' => 1,
+                    'icon' => 'fas fa-tachometer-alt',
+                    'menu_slug' => null,
+                    'menu_code' => 100,
+                    'menu' => null,
+                    'created_at' => $currentDateTime,
+                    'updated_at' => $currentDateTime,
+                ],
+                'children' => [
+                    [
+                        'name1' => 'Category',
+                        'name2' => 'Category',
+                        'sequence' => '10',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/category",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-download-cloud-2-fill',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Add New Product',
+                        'name2' => 'Add New Product',
+                        'sequence' => '20',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/products/create",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-arrow-go-back-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Product',
+                        'name2' => 'Product',
+                        'sequence' => '20',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/products",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-arrow-go-back-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                ]
             ],
             [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Role',
-                'name2' => 'Role',
-                'sequence' => '2',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/role",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-shield-user-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
+                'header' => [
+                    'name1' => 'Order-Management',
+                    'name2' => 'Order-Management',
+                    'is_active' => 1,
+                    'icon' => 'fas fa-tachometer-alt',
+                    'menu_slug' => null,
+                    'menu_code' => 200,
+                    'menu' => null,
+                    'created_at' => $currentDateTime,
+                    'updated_at' => $currentDateTime,
+                ],
+                'children' => [
+                    [
+                        'name1' => 'Order',
+                        'name2' => 'Order',
+                        'sequence' => '10',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/order",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-download-cloud-2-fill',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                ]
             ],
             [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Permission',
-                'name2' => 'Permission',
-                'sequence' => '3',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/permission",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-shield-keyhole-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'User Activity',
-                'name2' => 'User Activity',
-                'sequence' => '4',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/user-activity",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-user-follow-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Logged Users Tracking',
-                'name2' => 'Logged Users Tracking',
-                'sequence' => '5',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/logged-user-tracking",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-shield-keyhole-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Mail Tracking',
-                'name2' => 'Mail Tracking',
-                'sequence' => '6',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/mail-tracking",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-mail-check-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
-            ],
-            [
-                'menu_header_id' => $menuHeaderAdministration->id,
-                'name1' => 'Setting',
-                'name2' => 'Setting',
-                'sequence' => '7',
-                'menu_slug' => '',
-                'page_url' => "$prefix/administration/setting",
-                'is_submenu_available' => 0,
-                'is_active' => 1,
-                'icon' => 'ri-settings-5-line',
-                'created_at' => $currentDateTime,
-                'updated_at' => $currentDateTime,
+                'header' => [
+                    'name1' => 'Administration',
+                    'name2' => 'Administration',
+                    'is_active' => 1,
+                    'icon' => 'ri-admin-line',
+                    'menu_slug' => null,
+                    'menu_code' => 300,
+                    'menu' => json_encode(['item1' => 'value1', 'item2' => 'value2']),
+                    'created_at' => $currentDateTime,
+                    'updated_at' => $currentDateTime,
+                ],
+                'children' => [
+                    [
+                        'name1' => 'Users',
+                        'name2' => 'Users',
+                        'sequence' => '1',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/user",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-user-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Role',
+                        'name2' => 'Role',
+                        'sequence' => '2',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/role",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-shield-user-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Permission',
+                        'name2' => 'Permission',
+                        'sequence' => '3',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/permission",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-shield-keyhole-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'User Activity',
+                        'name2' => 'User Activity',
+                        'sequence' => '4',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/user-activity",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-user-follow-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Logged Users Tracking',
+                        'name2' => 'Logged Users Tracking',
+                        'sequence' => '5',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/logged-user-tracking",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-shield-keyhole-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ],
+                    [
+                        'name1' => 'Setting',
+                        'name2' => 'Setting',
+                        'sequence' => '7',
+                        'menu_slug' => '',
+                        'page_url' => "$prefix/administration/setting",
+                        'is_submenu_available' => 0,
+                        'is_active' => 1,
+                        'icon' => 'ri-settings-5-line',
+                        'created_at' => $currentDateTime,
+                        'updated_at' => $currentDateTime,
+                    ]
+                    //... continue for Permission, User Activity, etc.
+                ]
             ],
         ];
 
-        // MenuDetail::insert($menuDetails);
-
-        foreach ($menuDetails as $menuDetail) {
-            MenuDetail::updateOrCreate(
-                [
-                    // Match based on unique fields, e.g., `menu_header_id` and `menu_slug`
-                    'menu_header_id' => $menuDetail['menu_header_id'],
-                    // 'menu_slug' => $menuDetail['menu_slug'],
-                    'name1' => $menuDetail['name1'],
-                ],
-                [
-                    // Update or create with these values
-                    'name1' => $menuDetail['name1'],
-                    'name2' => $menuDetail['name2'],
-                    'sequence' => $menuDetail['sequence'],
-                    'page_url' => $menuDetail['page_url'],
-                    'is_submenu_available' => $menuDetail['is_submenu_available'],
-                    'is_active' => $menuDetail['is_active'],
-                    'icon' => $menuDetail['icon'],
-                    'created_at' => $menuDetail['created_at'],
-                    'updated_at' => $menuDetail['updated_at'],
-                ]
-            );
+        foreach ($menuItems as $item) {
+            $menuHeader = MenuHeader::create($item['header']);
+            if (!empty($item['children'])) {
+                foreach ($item['children'] as $child) {
+                    $child['menu_header_id'] = $menuHeader->id;
+                    MenuDetail::updateOrCreate(
+                        [
+                            'menu_header_id' => $menuHeader->id,
+                            'name1' => $child['name1'],
+                        ],
+                        $child
+                    );
+                }
+            }
         }
 
         generateMenuSlug();
